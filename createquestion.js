a7e9ba06bc0a7c395b0925adf4fef7916ad0b11f
Christoph Leuker <cleuker87@gmail.com>
2023-05-12 14:16:07 +0200

    add javascript for question

---
+++ createquestion.js
@@ -0,0 +1,8 @@
+const form = document.querySelector('[data-js="createform"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  console.log(data);
+});
