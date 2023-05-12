647aef49eaddf83a1929e09f47e81c973929106e
Christoph Leuker <cleuker87@gmail.com>
2023-05-12 10:59:54 +0200

    add bookmarkbutton and show answer js

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const bookmarkButton = document.querySelector('[data-js="bookmarkbutton"]');
-const filledBookmark = document.querySelector('[data-js="bookmarkjs]');
+const filledBookmark = document.querySelector('[data-js="bookmarkjs"]');
+const showAnswerButton = document.querySelector('[data-js="showAnswerButton"]');
+const answerShow = document.querySelector('[data-js="answer"]');
 
-/* bookmarkButton.addEventListener("click", () => {
-  filledBookmark.classList.add("bookmark__button--active");
-}); */
+bookmarkButton.addEventListener("click", () => {
+  filledBookmark.classList.toggle("bookmark__button--active");
+});
 
-function bm() {
+showAnswerButton.addEventListener("click", () => {
+  answerShow.classList.toggle("qcard__answer__show");
+  /* showAnswerButton.textContent = "Hide answer"; */
+  const test = showAnswerButton.textContent;
+  if (test === "Show answer") {
+    showAnswerButton.textContent = "Hide answer";
+  } else {
+    showAnswerButton.textContent = "Show answer";
+  }
+});
+
+/* function bm() {
   const addBookmark = document.getElementById("testbm");
   addBookmark.classList.toggle("bookmark__button--active");
 }
+ */
